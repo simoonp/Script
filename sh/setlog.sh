728363ffb6d49298f610a7bd1a829b19c0295a9e
明朋 <s202101013@stu.cqupt.edu.cn>
2022-09-20 21:43:39 +0800

    Update setlog.sh
    修改Windows换行符在Linux下异常的问题

--- sh/setlog.sh
+++ sh/setlog.sh
@@ -3,5 +3,8 @@ cd ~
 mkdir log
 # wget https://gitee.com/simoonp/rosdistro_backup/raw/master/log.bash 
 wget https://gitee.com/simoonp/rosdistro_backup/raw/master/log.sh 
+sudo apt install dos2unix 
+dos2unix log.sh
+
 chmod +x log.sh 
 sudo cp log.sh /etc/profile.d/ 
