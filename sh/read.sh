8f8326d39d842c016f66fb2088fac81141099ac6
明朋 <s202101013@stu.cqupt.edu.cn>
2022-09-30 20:16:58 +0800

    Update read.sh

--- sh/read.sh
+++ sh/read.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# 批量创建脚本，需要 info 文件
+# info 中保存 用户名、将要创建的容器名，映射的ssh端口、web端口，2个可选参数为镜像名和卷挂载位置
+# 例如：
+# gpu gyp 2751 8148
+# gpu ljy 2851 8248
+
 passwd=123456
 cat info | while read line
 do
