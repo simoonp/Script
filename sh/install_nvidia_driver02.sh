2d727a21e19420985563ca5a16c5b0e890dfc5f3
明朋 <s202101013@stu.cqupt.edu.cn>
2022-10-17 20:07:09 +0800

    Update install_nvidia_driver02.sh

--- sh/install_nvidia_driver02.sh
+++ sh/install_nvidia_driver02.sh
@@ -27,6 +27,27 @@ echo "注意检查文件夹是否存在"
 sleep 5s
 echo "export PATH=$cuda_path1:"'$PATH' >> $bashrc_path
 echo "export LD_LIBRARY_PATH=$cuda_path2:"'$LD_LIBRARY_PATH' >> $bashrc_path
+
+echo "安装cuDNN，请提前下载文件"
+sleep 5s
+"安装依赖"
+dpkg -i libcudnn8_*cuda*_amd64.deb
+dpkg -i libcudnn8-dev_*cuda*_amd64.deb
+dpkg -i libcudnn8-samples_*cuda*_amd64.deb
+
+echo "cuDNN测试"
+cp -r /usr/src/cudnn_samples_v8/ $HOME
+cd $HOME/cudnn_samples_v8/mnistCUDNN
+make clean 
+make
+./mnistCUDNN
+
+# 以下是卸载命令
+# sudo rm -rf /usr/local/cuda/include/cudnn.h
+# sudo rm -rf /usr/local/cuda/lib64/libcudnn*
+# sudo rm -rf /usr/include/cudnn.h
+# sudo rm -rf /usr/lib/x86_64-linux-gnu/libcudnn*
+
 echo "开启图形界面"
 sleep 5s
 systemctl set-default graphical.target
