9344ee279e1a6e8d6c9f60bcaa6109487aceabf5
明朋 <s202101013@stu.cqupt.edu.cn>
2023-04-17 10:05:46 +0800

    Create hashcat_bash.sh
    某些云容器上无法直接调用 hashcat，缺少 opengl

---
+++ sh/hashcat_bash.sh
@@ -0,0 +1,17 @@
+apt-get update && apt-get install -y --no-install-recommends \
+        ocl-icd-libopencl1 \
+        clinfo pkg-config
+        
+mkdir -p /etc/OpenCL/vendors && \
+    echo "libnvidia-opencl.so.1" > /etc/OpenCL/vendors/nvidia.icd
+    
+
+echo "/usr/local/nvidia/lib" >> /etc/ld.so.conf.d/nvidia.conf && \
+    echo "/usr/local/nvidia/lib64" >> /etc/ld.so.conf.d/nvidia.conf
+    
+apt-get update && \
+    apt-get install -y wget make clinfo build-essential git libcurl4-openssl-dev libssl-dev zlib1g-dev libcurl4-openssl-dev libssl-dev pciutils
+    
+update-pciids
+
+git clone https://github.com/hashcat/hashcat.git && cd hashcat && git checkout ${HASHCAT_VERSION} && make install -j4
